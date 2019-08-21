a5e75025fb6a57f0c9232e46c4e00d0a252ea172
Bryan McCann <bmac929@yahoo.com>
2019-08-21 13:45:40 -0500

    final edits

--- app/server.js
+++ app/server.js
@@ -6,14 +6,14 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({type: "application/*+json"}));
-app.use(bodyParser.raw({type: "application/vnd.custom-type"}));
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 //app.use(bodyParser.text({type: "text/html"}));
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
 
